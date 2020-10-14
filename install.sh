78713aa2888efbe6495820d58578b236dd845069
AXON <aminurfisa3@gmail.com>
2020-10-14 21:03:29 +0600

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,122 @@
+## Ready to use kernel installation script by @AXON
+## I strictly provide the rights to use this script with GearLock only.
+## For proper developer documentation, visit https://supreme-gamers.com/gearlock
+# Check `!zygote.sh` to configure your package functions or gearlock can also guide you during the build process.
+
+# Since GearLock 6.7.7 I decided to hold a native installation script inside gearlock/core instead.
+# To overcome the issue of needing to repack kernel packages just to update their install/uninstall scripts.
+# It's recommended that you use NATIVE_INSTALL, but if you prefer to add your own functions then you may set it to `false`. 
+NATIVE_INSTALL=true
+
+
+#####--- Import Functions ---#####
+get_base_dir # Returns execution directory path in $BD variable
+check_compat 6.7.7 # Returns yes in $COMPAT variable if the user is running at least 6.7.7 GearLock
+#####--- Import Functions ---#####
+
+
+# Load native scripts (When NATIVE_INSTALL is true)
+if test "$NATIVE_INSTALL" == "true"; then
+	rsync "$CORE/gxpm/mesa-native/uninstall.sh" "$BD/uninstall.sh"
+	rsync "$CORE/gxpm/mesa-native/install.sh" "$BD/install.sh" && exec "$BD/install.sh"
+fi
+
+
+
+# Since building a mesa library which would work on any android version is quite impossible.
+# Thus you must verify the host system android version. An example is given below (variable ref: https://supreme-gamers.com/gearlock/environment-variables)
+# # if [[ "$ANDROID_VER" =~ [4-7] ]]; then
+# #
+# #     geco "\n+ You can not install this mesa build in android-${ANDROID_VER}"
+# #     exit 101 #(exit-code ref: https://supreme-gamers.com/gearlock/#install-sh-exit-code)
+# #
+# # fi
+
+###
+### Start of un-native installer script (When NATIVE_INSTALL is false)
+###
+
+
+# Define vars
+DALVIKDIR="/data/dalvik-cache"
+MESA_BACKUP_DIR="$STATDIR/_mesaBackup"
+MESA_BACKUP_FILE="$STATDIR/mesa_stock.bak"
+GBSCRIPT="$GBDIR/init/ClearDalvikForKernelUpdate"
+
+# Define functions
+handleError ()
+{ 
+
+	if [ $? != 0 ]; then
+		# TODO: Revert back any incomplete changes
+		geco "\n[!!!] Error: $1" && exit ${2:-101}
+	fi
+
+}
+
+mesa_native()
+{
+	
+	"$CORE/gxpm/mesa-native/job" "$@"
+	
+}
+
+make_gbscript_clearDalvik ()
+{
+
+cat << EOF > "${GBSCRIPT}"
+
+## Dalvik cache cleaning gearboot script for live system installation
+######################################################################
+
+test -d "$DALVIKDIR" && geco "--+ Clearing dalvik-cache, it may take a bit long on this bootup" && rm -rf "$DALVIKDIR"/*
+rm "\$0"
+
+EOF
+
+}
+
+
+##
+## Main
+##
+
+# Do not allow GearLock versions below 6.7.7
+# # if ! check_compat 6.7.7; then geco "+\n Please update GearLock to install this"; exit 101; fi
+test "$COMPAT" != "yes" && geco "\n[!!!] Please update GearLock to install this" && exit 101
+
+# Warning info for installation from GUI to avoid system crash
+test "$BOOTCOMP" == "yes" && geco "[!!!] You seem to be installing from a live system, best practice is to install from RECOVERY-MODE.\n"
+
+# Check if /system is writable
+! touch -c "$SYSTEM_DIR/lib" >/dev/null 2>&1 && geco "[!!!] $SYSTEM_DIR is not writable, did you ${PINK}SuperCharge${RC} it yet ?" && exit 101
+
+# Backup mesa
+if [ ! -f "$MESA_BACKUP_FILE" ]; then
+	geco "\n+ Backing up stock Mesa dri & dependencies ..."
+	mesa_native backup "$MESA_BACKUP_DIR"
+	geco ">>>>>> Compressing backup ..."
+	( cd "$MESA_BACKUP_DIR"; tar --zstd -cpf "$MESA_BACKUP_FILE" system ); handleError "Failed to backup current Mesa"
+	rm -rf "$MESA_BACKUP_DIR"
+fi
+
+# Cleanup mesa
+geco "\n+ Cleaning up existing Mesa dri & dependencies ..." && mesa_native clean
+
+# Merge mesa
+geco "\n+ Merging new Mesa dri & dependencie files in your operating-system"
+gclone "$BD/system" "$SYSTEM_DIR"; handleError "Failed to place files"
+
+# Symlink dri
+geco "\n+ Symlinking dri directories if needed ..." && sleep 1
+for libX in lib lib64; do
+	[ -e "$SYSTEM_DIR/$libX/dri" ] && [ ! -e "$SYSTEM_DIR/vendor/$libX/dri" ] && ln -srf "$SYSTEM_DIR/$libX/dri" "$SYSTEM_DIR/vendor/$libX/dri"
+	[ -e "$SYSTEM_DIR/vendor/$libX/dri" ] && [ ! -e "$SYSTEM_DIR/$libX/dri" ] && ln -srf "$SYSTEM_DIR/vendor/$libX/dri" "$SYSTEM_DIR/$libX/dri"
+done
+
+# Clear dalvik-cache
+if test "$TERMINAL_EMULATOR" != "yes" && test -d "$DALVIKDIR"; then
+	geco "\n+ Clearing dalvik-cache, it may take a bit long on your next boot" && rm -rf "$DALVIKDIR"/*
+elif test "$TERMINAL_EMULATOR" == "yes" && test -d "$DALVIKDIR"; then
+	make_gbscript_clearDalvik
+fi
---
+++ uninstall.sh
@@ -0,0 +1,65 @@
+## Ready to use mesa uninstallation script by @AXON
+## For proper developer documentation, visit https://supreme-gamers.com/gearlock
+# You don't need to modify this `uninstall.sh`.
+# 
+# $UNINS_SCRIPT variable is provided by GXPM which returns
+# full path for the locally prepared uninstallation script.
+
+
+# Define vars
+DALVIKDIR="/data/dalvik-cache"
+MESA_BACKUP_FILE="$STATDIR/mesa_stock.bak"
+GBSCRIPT="$GBDIR/init/ClearDalvikForKernelUpdate"
+
+# Define functions
+handleError ()
+{ 
+
+	if [ $? != 0 ]; then
+		# TODO: Revert back any incomplete changes
+		geco "\n[!!!] Error: $1" && exit ${2:-101}
+	fi
+
+}
+
+mesa_native()
+{
+	
+	"$CORE/gxpm/mesa-native/job" "$@"
+	
+}
+
+# Deny uninstallation from GUI to avoid system crash
+if [ "$TERMINAL_EMULATOR" == "yes" ]; then
+	geco "\n+ You can not uninstall Mesa from Android GUI, it will crash your system."
+	geco "+ It is not recommended that you uninstall from a live system, best practice is to uninstall from RECOVERY-MODE."
+	geco "+ You can still run GearLock in ${PURPLE}TTY${RC} and uninstall from there but it's not recommended.\n"
+	while true
+	do
+		read -n1 -p "$(geco "Do you want to switch to ${BGREEN}TTY${RC} and uninstall from there ? [${GREEN}Y${RC}/n]") " i
+		case $i in
+					
+			[Yy] ) geco "\n\n+ Switching to TTY GearLock GXPM ..." && sleep 2
+					gsudo openvt -s gxpm -u "$UNINS_SCRIPT"; return 101; break ;;
+						
+			[Nn] ) geco "\n\n+ Okay, uninstallation process will exit"
+					return 101; break ;;
+						
+				*) geco "\n- Enter either ${GREEN}Y${RC}es or no" ;;
+					
+		esac
+	done
+fi
+
+# Check if /system is writable
+! touch -c "$SYSTEM_DIR/lib" >/dev/null 2>&1 && geco "[!!!] $SYSTEM_DIR is not writable, did you ${PINK}SuperCharge${RC} it yet ?" && exit 101
+
+# Check for backup archive existence
+test -e "$MESA_BACKUP_FILE"; handleError "Mesa backup archive not found"
+
+# Cleanup mesa
+geco "\n+ Cleaning up current Mesa dri & dependencies ..." && mesa_native clean
+
+# Restore backup mesa
+geco "\+ Restoring stock Mesa from backup archive ..."
+tar --zstd --strip-components=1 --directory="$SYSTEM_DIR" -xpf "$MESA_BACKUP_FILE"; handleError "Failed to restore stock Mesa from backup archive"
